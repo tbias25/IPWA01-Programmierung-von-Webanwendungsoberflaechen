dff7ac5b25e6f9eecfbc399a5731bbc20fb104b3
Tobias <55360690+tbias25@users.noreply.github.com>
2023-11-03 19:26:10 +0100

    added input component from daisyui

--- rewear-app/src/components/input-form-card.js
+++ rewear-app/src/components/input-form-card.js
@@ -1,16 +1,29 @@
 /*
     https://daisyui.com/components/card/
     card with image overlay
+
+    https://daisyui.com/components/input/
+    with form-control and labels
 */
 
 export default function InputFormCard() {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <div className="card-body">
-        <h2 className="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">What is your name?</span>
+            <span className="label-text-alt">Top Right label</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <label className="label">
+            <span className="label-text-alt">Bottom Left label</span>
+            <span className="label-text-alt">Bottom Right label</span>
+          </label>
         </div>
       </div>
     </div>
