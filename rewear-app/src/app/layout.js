d91cc617c47ab54898d784a1efd84b943f4d7445
Tobias <55360690+tbias25@users.noreply.github.com>
2023-11-03 20:11:49 +0100

    added the footer from daisyui

--- rewear-app/src/app/layout.js
+++ rewear-app/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       {/* added the navbar */}
       <Navbar />
       {children}
+      <Footer />
       </body>
     </html>
   )
